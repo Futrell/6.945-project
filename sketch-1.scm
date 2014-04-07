c9bca71d8c399915d97586c52a8bca922f37e85a
jduyck <jamesduyck@gmail.com>
2014-04-07 00:30:13 -0300

    Update sketch-1.scm

--- sketch-1.scm
+++ sketch-1.scm
@@ -27,7 +27,7 @@
 	 semantics ; this is the universe's representation of events,
 		   ; which the from-agent will receive in its own way,
 		   ; encode using the grammar, and send to the
-		   ; two-agent, who will try to decode it
+		   ; to-agent, who will try to decode it
 	 )
 
   (let* ((encoded-message (agent-encode alice semantics))
@@ -44,8 +44,47 @@
       (backchannel from-agent to-agent semantics decoded-semantics)
       'ok)))
 
+;;; J: I think we want to be more general than this. 
+;;; Beal's system is conceptually more like both agents speaking at
+;;; once, with one agent being chosen to be successful (I think it
+;;; has to do with how he models the brain?). 
+;;; I think we could do something like this (*might be overly OOP*):
+(define (one-full-cycle
+         ordered-agent-set
+         semantics ; as above
+         comm-channel ; this is the channel between agents
+                      ; it handles adding noise and passing messages
+         settings ; ex. training vs. communicating
+         )
+  (set! clock (+ clock 1))
+  (agents-read ordered-agent-set semantics settings)
+    ;; typically agents read streams.
+    ;; whether each agent reads every time, every other, when they
+    ;; need to, etc. depends on implementation
+    ;; conceptually, what an agent 'hears' may not be controlled by
+    ;; any other part of the system
+  (transmit comm-channel ordered-agent-set settings)
+    ;; comm-channel calls agent-encode as above, does whatever
+    ;; processing it does, then passes filtered messages to agents
+  (agents-process ordered-agent-set settings)
+    ;; agents all call agent-decode on their internal state,
+    ;; which might have a recent message or an indicator from
+    ;; comm-channel telling them to do nothing
+    ;; agents may update internal state
+  (agents-output ordered-agent-set settings) ; probably just prints
+  (universe-update settings semantics clock)
+)
+;;; If this is too OOP we could have each step call the next.
+
 ;;; Question: How much state do we want to have? Should this function
 ;;; return new values for alice and bob or mutate them?  
+
+    ;;; J: I think that given what we are modelling (states in the
+    ;;; brain, physical devices swarms) mutation is more intuitive.
+    ;;; It also depends on how we implement alice and bob.
+    ;;; I guess they are probably tagged lists? Do we want agents
+    ;;; to drag along functions, or do we use generic operators
+    ;;; that depend on which agent-tag is present?
 
 ;;; What is becoming clear to me is that each agent needs two
 ;;; different interfaces: (1) with the universe and (2) with the other
@@ -63,6 +102,9 @@
 ;;; The fact that each of these interfaces needs to be bidirectional
 ;;; suggests we could use propagators as one implementation. 
 ;;;
+    ;;; J: Good idea. I think it would work event if agents have
+    ;;; different grammars.
+
 ;;; The other big thing the agents need is a way to update their
 ;;; languages. Maybe that could be wrapped up into the language object
 ;;; too.
@@ -94,4 +136,8 @@
 ;;; constructors, or we could force the user to make encoding and
 ;;; decoding procedures to interface with that network. 
 
-
+    ;;; J: I think that the languages should at least be separate 
+    ;;; instances, so agents could have different languages. 
+    ;;; Whether we move the difference in implementations into
+    ;;; agent or language should depend on what's easier to code. I
+    ;;; don't think it makes much conceptual difference.
