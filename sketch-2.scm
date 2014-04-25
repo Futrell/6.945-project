5be44209ee4be9d1092027c4770cc012ccc22373
James A Duyck <jduyck@mit.edu>
2014-04-25 01:41:20 -0400

    I have begun to make a separate feedback functionn per agent. I decided that the easiest way to do that was to have the agents store some history, and to have the channels call the feedback functions.

--- sketch-2.scm
+++ sketch-2.scm
@@ -33,6 +33,34 @@
 ;;; The universe interfaces are fixed. The task is to learn the
 ;;; communication interfaces. 
 
+
+;;; UTILS
+
+(define (list-set-at! lst n val)
+  (set-car! (list-tail lst n) val)) 
+  ;; http://stackoverflow.com/questions/7382117
+
+(define dict-end '(end# end#))
+
+(define (dict) (list dict-end))
+
+(define (dict-put! alist key val) 
+  ;; Adds a value to a key, replacing previous values for
+  ;; the key 
+  (if (and (assq key alist) 
+           (not (eq? (assq key alist) dict-end)))
+      (let ((dict-mem (member-procedure 
+                        (lambda (ele obj) (eq? obj (car ele))))))
+        (set-car! (dict-mem key alist) (list key val)))
+      (begin
+        (set-car! (list-tail alist (- (length alist) 1))
+                  (list key val))
+        (set-cdr! (list-tail alist (- (length alist) 1))
+                  (list dict-end)))))
+
+
+;;;
+
 (define gensym generate-uninterned-symbol)
 
 ;;; let's do the simplest possible mockup
@@ -55,12 +83,12 @@
 (define (make-simple-language) 
   (define grammar '())
 
-  (define respond-to-feedback! add-meaning-message-pair-to-grammar!)
-
   (define (add-meaning-message-pair-to-grammar! meaning message)
     (set! grammar (cons (list meaning message) grammar))
     (pp "new grammar:")
     (pp grammar))
+
+  (define respond-to-feedback! add-meaning-message-pair-to-grammar!)
 
   (define (dont-know-message-for-meaning meaning)
     (let ((made-up-message (gensym)))
@@ -121,13 +149,80 @@
 (define (get-update-grammar-proc language)
   (list-ref language 3))
 
+(define (set-grammar! language new-grammar)
+  (list-set-at! language 0 new-grammar))
+
+(define (set-encode-proc! language new-encode-proc)
+  (list-set-at! language 1 new-encode-proc))
+
+(define (set-decode-proc! language new-decode-proc)
+  (list-set-at! language 2 new-decode-proc))
+
+(define (set-update-grammar-proc! language new-update-grammar-proc)
+  (list-set-at! language 3 new-update-grammar-proc))
+
+
+;;; HISTORY
+;;; Used in AGENT to store past data which is used for feedback.
+;;;    Note that conceptually history is more related to the universe,
+;;;    in particular it may store signals that the agent would only
+;;;    see through a noisy perceive-proc
+;;; The basic history here is a list of:
+;;; 0: The most recent event that the agent received from the universe
+;;; 1: The meaning interpreted of that event
+;;; 2: The message computed of that meaning
+;;; 3: An alist of channels, and the most recent message the agent
+;;;    received from the channel.
+;;; 4: An alist of channels, and the meanings computed from the
+;;;    messages.
+;;; 5: An alist of channels, and the events interpreted from the
+;;;    meanings.
+
+
+(define (make-basic-history)
+  (list '() '() '() (dict) (dict) (dict)))
+
+(define (get-last-event-in history) first)
+(define (get-last-e-meaning history) second)
+(define (get-last-message-out history) third)
+
+(define (get-last-message-in history channel)
+  (cadr (assq (fourth history) channel)))
+(define (get-last-m-meaning history channel)
+  (cadr (assq (fifth history) channel)))
+(define (get-last-event-out history channel)
+  (cadr (assq (sixth history) channel)))
+
+(define (set-last-event-in! history new-ei) 
+  (list-set-at! history 0 new-ei))
+(define (set-last-e-meaning! history new-em)
+  (list-set-at! history 1 new-em))
+(define (set-last-message-out! history new-mo) 
+  (list-set-at! history 2 new-mo))
+
+(define (set-last-message-in! history channel new-mi)
+  (dict-put! (fourth history) channel new-mi))
+(define (set-last-m-meaning! history channel new-mm)
+  (dict-put! (fifth history) channel new-mm))
+(define (set-last-event-out! history channel new-eo)
+  (dict-put! (sixth history) channel new-eo))
+
 
 ;;; AGENT
 
+;;; An agent is a list of:
+;;; 0: a language (see above)
+;;; 1: perceive-proc converts an event from the universe into a
+;;;    meaning.
+;;; 2: interpret-proc converts a meaning into a signal to the
+;;;    universe. 
+;;; 3: history (see above)
+
 (define (make-agent language
 		    perceive-proc
-		    interpret-proc)
-  (list language perceive-proc interpret-proc))
+		    interpret-proc
+                    history)
+  (list language perceive-proc interpret-proc history))
 
 (define (get-language agent)
   (list-ref agent 0))
@@ -135,13 +230,37 @@
 (define (get-perceive-proc agent)
   (list-ref agent 1))
 
-(define (get-interpret-proc agent)
+(define (get-interpret-proc agent) ;; Need a better name here
+                                   ;; Conceptually, perceive backwards
   (list-ref agent 2))
+
+(define (get-history agent)
+  (list-ref agent 3))
+
+(define (set-history! agent new-history)
+  (list-set-at! agent 4 new-history))
 
 (define (make-simple-agent)
   (make-agent (make-simple-language)
 	      (lambda (x) x)
-	      (lambda (x) x)))
+	      (lambda (x) x)
+              (make-basic-history)))
+
+
+;;; CHANNEL
+
+(define (make-channel channel-model agent-1 agent-2)
+  (list channel-model agent-1 agent-2))
+
+(define (get-channel-model) first)
+(define (get-channel-agent1) second)
+(define (get-channel-agent2) third)
+
+(define (channel-transmit)
+  )
+
+(define (channel-give-feedback channel parameters)
+  )
 
 
 ;;; EXPERIMENTAL SETUP
@@ -157,7 +276,7 @@
   (iterate 0))
 
 (define (make-experiment agents 
-			 channel 
+			 channel-model 
 			 sample-universe-event)
   (define clock 0)
 
@@ -208,8 +327,7 @@
 					  parameters))
 	 (interpretations (agents-interpret agents decoded-meanings 
 					    parameters))
-	 (feedback-signals (get-feedback agents meanings decoded-meanings 
-					 sent-messages received-messages
+	 (feedback-signals (get-feedback agents received-messages
 					 event interpretations
 					 parameters))) ; what is
 					; the best way to specify how
@@ -286,3 +404,14 @@
 ;;; Generalize the language interface XX
 ;;; Names: Write a document to standardize. 
 
+
+;;; I think that it would make sense to have a couple levels of
+;;; complexity (sort of thinking along the lines of the Making Magic
+;;; article Robert cited). 
+;;; In particular, there are some elements of the agents that most
+;;; people won't want to change, so we can have them more strongly
+;;; built-in (i.e. most programmers don't see them).
+;;; The main case that I'm thinking of is only having agents remember
+;;; one time-step of the past. If someone really wants more they can
+;;; alter the agent code or the way 'grammar' works, but otherwise
+;;; there's just one available.
